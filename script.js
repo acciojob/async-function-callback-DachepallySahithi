a9cc5f5c160fedd1a435c7bf41973531206d3271
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-03-06 10:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+async function fetchDataAndDisplay(callback) {
+	try{
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		callback(data.title);
+	}
+	catch(error){
+		console.error("Error fectching data:", error);
+	}
+}
+function displayTitle(title){
+	const outputDiv = document.getElementById('output');
+	outputDiv.textContent = title;
+}
+document.getElementById('btn').addEventListener('click', () => {
+    fetchDataAndDisplay(displayTitle);
+});
